37ff6f636f533dfb74bff0dae0956a94efde44d2
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-01-17 18:57:53 -0300

    Nova página de index
    Criado novo HTML e CSS  com a página de boas vindas

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,120 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A layout example that shows off a responsive product landing page.">
+    <title>Rangão</title>
+    <link rel="shortcut icon" href="img/icon/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="css/welcome.css">
+</head>
+<body>
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="/">
+            <img src="img/icon/favicon.ico" alt="Icone rangão" width="32">Rangão</a>
+
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item"><a href="/register" class="pure-button">Cadastre-se</a></li>
+            <li class="pure-menu-item"><a href="/home" class="pure-button button-success">Entrar</a></li>
+        </ul>
+    </div>
+</div>
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Se alimente bem e cuide do mundo!</h1>
+        <p>
+            <a href="/home" class="pure-button button-success button-xlarge">Entrar</a>
+        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+    <div class="content">
+        <h2 class="content-head is-center">Por que fazemos isso?.</h2>
+
+        <div class="pure-g">
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+
+                <h3 class="content-subhead">
+                    <i class="fa fa-rocket"></i>
+                    Animais
+                </h3>
+                <p>
+                    São abatidos mais de 10 mil animais terrestres por minuto no Brasil para produzir carnes, leite e
+                    ovos. A maioria destes animais são frangos, porcos e bois – animais que têm uma complexa capacidade
+                    cognitiva e sentem dor, sofrimento e alegria da mesma forma que os cães que temos em casa. Os
+                    animais são sencientes (capazes de sofrer e sentir prazer e felicidade), por isso a escolha
+                    vegetariana é uma escolha de não compactuar com a exploração, confinamento e abate destes animais.
+                    <a href="https://www.svb.org.br/vegetarianismo1/etica-animal" class="pure-menu-link">(Saiba
+                        mais)</a>
+                </p>
+            </div>
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 class="content-subhead">
+                    <i class="fa fa-mobile"></i>
+                    Saúde
+                </h3>
+                <p>
+                    Diversos estudos associam efeitos positivos de saúde com a maior utilização de produtos de origem
+                    vegetal e restrição de produtos oriundos do reino animal. De acordo com inúmeros estudos científicos
+                    – cada vez mais freqüentes e publicados por instituições idôneas –, o consumo de carnes está
+                    diretamente associado ao risco aumentado de doenças crônicas e degenerativas como diabetes,
+                    obesidade, hipertensão e alguns tipos de câncer.
+                    <a href="https://www.svb.org.br/vegetarianismo1/saude" class="pure-menu-link">(Saiba mais)</a>
+                </p>
+            </div>
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 class="content-subhead">
+                    <i class="fa fa-th-large"></i>
+                    Meio ambiente
+                </h3>
+                <p>
+                    Segundo a ONU, o setor pecuário é o maior responsável pela erosão de solos e contaminação de
+                    mananciais aqüíferos do mundo. A ONU também estimou que cerca de 14,5% das emissões de gases do
+                    efeito estufa oriundas de atividades humanas têm origem no setor pecuário. A maior parte do
+                    desmatamento da Amazônia tem sua origem na produção de carnes, laticínios e ovos. 97% do farelo de
+                    soja e 60% do milho produzidos globalmente são utilizados não para consumo humano, mas para virar
+                    ração para as fazendas e granjas industriais, produzindo alimentos a uma eficiência muito baixa.
+                    <a href="https://www.svb.org.br/vegetarianismo1/meio-ambiente" class="pure-menu-link">(Saiba
+                        mais)</a>
+                </p>
+            </div>
+            <div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+                <h3 class="content-subhead">
+                    <i class="fa fa-check-square-o"></i>
+                    Sociedade
+                </h3>
+                <p>
+                    A produção de alimentos através da atividade pecuária não é apenas ambientalmente degradante, mas
+                    também contribui significativamente para o desperdício global de alimentos, uma vez que são
+                    consumidos de 2 a 10 Kg de proteína vegetal (por exemplo, soja) para produzir apenas 1 Kg de
+                    proteína de origem animal. Em um mundo com 1 bilhão de pessoas que passam fome, jogar toda essa
+                    comida no lixo é socialmente inaceitável. Ademais, o setor pecuário concentra a maior parte da
+                    mão-de-obra escrava rural brasileira.
+                    <a href="https://www.svb.org.br/vegetarianismo1/dignidade-humana" class="pure-menu-link">(Saiba
+                        mais)</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="ribbon l-box-lrg pure-g">
+        <div class="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
+            <img width="64" alt="File Icons" class="pure-img-responsive" src="img/icon/favicon.ico">
+        </div>
+        <div class="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
+            <h2 class="content-head content-head-ribbon">Rangão.</h2>
+            <p>
+                Esse é um projeto para desenvolver e aplicar meus conhecimentos com PHP e Laravel, você pode me
+                encontrar
+                nas redes sociais por <a href="https://github.com/devcapu" class="pure-menu-link">@devcapu</a>
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+>>>>>>> Stashed changes
