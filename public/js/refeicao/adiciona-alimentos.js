e88b01766f906d0d05ed0e0b010916c51b4979db
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-01-21 14:52:52 -0300

    Adicionando os alimentos na table
    Passando os alimentos selecionados na tabela de sua refeição

---
+++ public/js/refeicao/adiciona-alimentos.js
@@ -0,0 +1,22 @@
+(function () {
+    const formAlimento = document.querySelector('#adicionaAlimento');
+
+    formAlimento.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const refeicaoEscolhida = document.querySelector('#refeicao').value;
+        const selectAlimentoEscolhido = document.querySelector('#alimentos');
+        const alimentoEscolhido = selectAlimentoEscolhido.value;
+        const optionAlimentoEscolhido = selectAlimentoEscolhido.querySelector(`option[value="${alimentoEscolhido}"]`);
+        const optionAlimentoEscolhidoTexto = optionAlimentoEscolhido.textContent;
+        const quantidade = document.querySelector('#quantidade').value;
+        const tabela = document.querySelector(`#tabela-${refeicaoEscolhida} tbody`);
+        const novaTr = document.createElement('tr');
+        const tdNome = document.createElement("td");
+        const tdQuantidade = document.createElement("td");
+        tdNome.textContent = optionAlimentoEscolhidoTexto;
+        tdQuantidade.textContent = quantidade;
+        novaTr.appendChild(tdNome);
+        novaTr.appendChild(tdQuantidade);
+        tabela.appendChild(novaTr);
+    })
+})();
--- resources/views/refeicao/create.blade.php
+++ resources/views/refeicao/create.blade.php
@@ -8,7 +8,7 @@
     <title>Rangão</title>
 </head>
 <body>
-<form action="#">
+<form action="#" id="adicionaAlimento">
     <label for="refeicao">Escolha uma refeição
         <select name="refeicao" id="refeicao">
             <option value="cafeDaManha">Café da manhã</option>
@@ -26,11 +26,62 @@
     <label for="quantidade">Quanto que você comeu desse alimento?
         <input type="number" name="" id="quantidade" step="1" min="1" max="100">
     </label>
-    <button>Adicionar alimento</button>
+    <button id="button-adiciona">Adicionar alimento</button>
 </form>
-<table>
-
+<table id="tabela-cafeDaManha">
+    <thead>
+    <tr>
+        <th colspan="2">Café da manhã</th>
+    </tr>
+    <tr>
+        <td>Nome</td>
+        <td>Quantidade</td>
+    </tr>
+    </thead>
+    <tbody></tbody>
 </table>
+
+<table id="tabela-almoco">
+    <thead>
+    <tr>
+        <th colspan="2">Almoço</th>
+    </tr>
+    <tr>
+        <td>Nome</td>
+        <td>Quantidade</td>
+    </tr>
+    </thead>
+    <tbody></tbody>
+</table>
+
+<table id="tabela-cafeDaTarde">
+    <thead>
+    <tr>
+        <th colspan="2">Café da tarde</th>
+    </tr>
+    <tr>
+        <td>Nome</td>
+        <td>Quantidade</td>
+    </tr>
+    </thead>
+    <tbody></tbody>
+</table>
+
+<table id="tabela-jantar">
+    <thead>
+    <tr>
+        <th colspan="2">Janta</th>
+    </tr>
+    <tr>
+        <td>Nome</td>
+        <td>Quantidade</td>
+    </tr>
+    </thead>
+    <tbody></tbody>
+</table>
+
+<button id="salvarRefeições">Salvar cardápio</button>
 <script src="{{asset('js/refeicao/busca-alimentos.js')}}"></script>
+<script src="{{asset('js/refeicao/adiciona-alimentos.js')}}"></script>
 </body>
 </html>
