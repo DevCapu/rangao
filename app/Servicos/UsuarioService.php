fd2f65384f2942c21bc19163b543b86f03efbbae
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-01-28 18:27:02 -0300

    Buscando refeições do usuário
    * Fazendo query para buscar refeições daquele dia do usuário logado
    * Formatando data Atual com o carbon

--- app/Servicos/UsuarioService.php
+++ app/Servicos/UsuarioService.php
@@ -4,8 +4,11 @@
 namespace App\Servicos;
 
 
+use Carbon\Carbon;
 use DevCapu\NutriLive\App\PatientCalculator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UsuarioService
@@ -43,5 +46,31 @@ class UsuarioService
             "caloriasParaConseguirObjetivo" => $caloriasParaConseguirObjetivo,
             "foto" => ""
         ];
+    }
+
+    public function buscaRefeicoesDoDiaAtual()
+    {
+        $refeicoesDoDia = DB::table('refeicoes')
+            ->join('refeicao_alimentos', 'refeicao_alimentos.id_refeicao', '=', 'refeicoes.id')
+            ->join('alimentos', 'alimentos.id', '=', 'refeicao_alimentos.id_alimento')
+            ->select(
+                'refeicao_alimentos.id',
+                'refeicoes.periodo',
+                'alimentos.nome',
+                'alimentos.calorias',
+                'refeicao_alimentos.quantidade',
+                )
+            ->where(['data' => $this->retornaDataAtualFormatada(), 'id_usuario' => Auth::id()])
+            ->get();
+
+        return $refeicoesDoDia;
+    }
+
+    private function retornaDataAtualFormatada()
+    {
+        $dataDeHoje = explode(' ', Carbon::now('America/Sao_Paulo')->subDay());
+        $dataDeHoje = join('/', array_reverse(explode('-', $dataDeHoje[0])));
+
+        return $dataDeHoje;
     }
 }
