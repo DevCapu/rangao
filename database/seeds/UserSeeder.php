2f439a42738a7106e68db06ab0113cd3f542833f
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-04-19 22:51:01 -0300

    Seeder se usuário
    Populando tabela de usuário

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            "name" => "Felipe Moreno Borges",
+            "birthday" => "15/10/2000",
+            "gender" => "male",
+            "objective" => "lose",
+            "email" => "admin@gmail.com",
+            "password" => Hash::make("admin1234"),
+            "height" => 1.78,
+            "weight" => 76.15,
+            "activity" => 'littleActive',
+            "basalEnergyExpenditure" => 1022.60,
+            "totalEnergyExpenditure" => 1533.90,
+            "caloriesToCommitObjective" => 1181.10,
+            "photo" => ""
+        ]);
+    }
+}
