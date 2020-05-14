ac68d0a4ab389f1161e01077f18230b91f0cd8ce
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-05-14 14:25:05 -0300

    Adicionando um alimento na geladeira e validando a request

--- app/Http/Controllers/RefrigeratorController.php
+++ app/Http/Controllers/RefrigeratorController.php
@@ -4,7 +4,9 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\Refrigerator;
+use App\Http\Requests\SaveFoodInsideRefrigeratorRequest;
+use App\Models\FoodRefrigerator;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class RefrigeratorController
@@ -17,5 +19,23 @@ class RefrigeratorController
             'foods' => $foods,
             'userId' => Auth::id()
         ]);
+    }
+
+    public function create()
+    {
+        return view('refrigerator.create');
+    }
+
+    public function store(SaveFoodInsideRefrigeratorRequest $request, FoodRefrigerator $foodRefrigerator)
+    {
+        $foodRefrigerator->food_id = $request->food;
+        $foodRefrigerator->refrigerator_id = Auth::user()->refrigerator->id;
+        $foodRefrigerator->quantity = $request->quantity;
+        $foodRefrigerator->expiration_date = $request->expiration_date;
+        $foodRefrigerator->notification = $request->notification;
+
+        $foodRefrigerator->save();
+
+        return redirect()->route('refrigerator.index');
     }
 }
