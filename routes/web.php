923047d1a718cfb7e64a11b6be61cada38d095ea
Felipe Moreno Borges <cyberlegend48@gmail.com>
2020-05-21 22:06:31 -0300

    Corrigindo os links do perfil

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,39 @@
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('usuario.login');
-});
+/*Base*/
+Route::redirect('/', '/login')->name('index');
 
-Auth::routes();
+/*Criar um novo usuário*/
+Route::get('/register', 'UserController@create')->name('users.create');
+Route::post('/users', 'UserController@store')->name('users.store');
 
-Route::resource('alimento', 'AlimentoController')->middleware('auth');
-Route::resource('refeicao', 'RefeicaoController')->middleware('auth');
-Route::resource('usuario', 'UsuarioController');
+/*Fazer login*/
+Route::get('/login', 'AuthController@login')->name('users.login');
+Route::post('/login', 'AuthController@doLogin')->name('users.login.do');
 
-Route::get('/perfil', 'UsuarioController@perfil')->middleware('auth')->name('perfil');
-Route::get('/login', 'AuthController@login')->name('usuario.login');
-Route::post('/login', 'AuthController@doLogin')->name('usuario.login.do');
-Route::get('/logout', 'AuthController@logout')->name('usuario.logout')->middleware('auth');
+/*Fazer logout*/
+Route::get('/logout', 'AuthController@logout')->name('users.logout')->middleware('auth');
+
+/*Perfil*/
+Route::get('/profile/{user}', 'UserController@profile')->name('profile')->middleware('auth');
+Route::get('user/{user}/edit', 'UserController@edit')->name('users.edit')->middleware('auth');
+
+/*Alimentos da geladeira*/
+Route::get('/refrigerator', 'RefrigeratorController@index')->name('refrigerator.index')->middleware('auth');
+Route::get('/refrigerator/create', 'RefrigeratorController@create')->name('refrigerator.create')->middleware('auth');
+Route::post('/refrigerator', 'RefrigeratorController@store')->name('refrigerator.store')->middleware('auth');
+Route::get('/refrigerator/{foodId}/edit', 'RefrigeratorController@edit')->name('refrigerator.edit')->middleware('auth');
+
+/*Alimentos ingeridos*/
+Route::get('meal/create', 'MealController@index')->name('meal.create')->middleware('auth');
+
+/*Feed*/
+Route::get('/feed', 'FeedController@index')->name('feed.index')->middleware('auth');
+
+/*Tips*/
+Route::get('/tips', 'TipController@index')->name('tips.index')->middleware('auth');
+
+/*Menu*/
+Route::post('/menu/generate', 'MenuController@generate')->name('menu.generate')->middleware('auth');
+
